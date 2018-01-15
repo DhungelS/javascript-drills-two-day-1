9d56fef7a0530f6c678e7a4c24c1de29c81a69f0
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-15 17:16:42 -0500

    "last commit for the day"
    :wq

--- script.js
+++ script.js
@@ -47,69 +47,97 @@ else {
 }
 console.log(decode("droop"))*/
 
-function daysInMonth(month) {
-    switch (month) {
-        case "January":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+// function daysInMonth(month) {
+//     switch (month) {
+//         case "January":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "February":
-            days = 28;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "February":
+//             days = 28;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "March":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "March":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "April":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "April":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "May":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "May":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "June":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "June":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "July":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "July":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "August":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "August":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "September":
-            days = 30;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "September":
+//             days = 30;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "October":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "October":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "November":
-            days = 30;
-            console.log(`${month} has ${days} days.`)
-            break;
+//         case "November":
+//             days = 30;
+//             console.log(`${month} has ${days} days.`)
+//             break;
 
-        case "December":
-            days = 31;
-            console.log(`${month} has ${days} days.`)
-            break;
-    }
+//         case "December":
+//             days = 31;
+//             console.log(`${month} has ${days} days.`)
+//             break;
+//     }
+// }
+
+
+// daysInMonth("November")
+
+function rockPaperScissors(rps){
+const randomNo = Math.floor(Math.random() * 3) + 1;
+console.log(`Computers current number is ${randomNo}`)
+if(randomNo === rps ){
+console.log("It's a tie!")
+}
+else if(randomNo === 1 && rps === 3){
+  console.log("Computer wins!")
+}
+else if(randomNo === 3 && rps === 1){
+  console.log("You win!")
+}
+else if(randomNo === 1 && rps === 2){
+  console.log("You win!")
+}
+else if(randomNo === 2 && rps === 1){
+  console.log("Computer wins!")
+}
+else if(randomNo === 2 && rps === 3){
+  console.log("You win")
+}
+else if(randomNo === 3 && rps === 2){
+  console.log("Computer wins")
+}
 }
 
-
-daysInMonth("November")
+rockPaperScissors(3)
