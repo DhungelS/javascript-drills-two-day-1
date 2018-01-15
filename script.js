d3948a3c8a00c89756b063417188c46f746c2d65
Brian Dudey <dudey.brian@gmail.com>
2018-01-15 14:59:52 -0600

    Second Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function jediName (firstName, lastName){
+/*function jediName (firstName, lastName){
   const firstHalf = firstName.slice(0,2)
   const secondHalf = lastName.slice(0,3)
   console.log(firstHalf)
@@ -6,4 +6,21 @@ function jediName (firstName, lastName){
   console.log(secondHalf + firstHalf)
 }
 
-jediName("Saugat", "Dhungel")
+jediName("Saugat", "Dhungel")*/
+
+function beyond(num) {
+    if (num === Infinity) {
+        console.log("And beyond");
+    } else if (num !== Infinity) {
+        console.log("To infinity");
+    } else if (num < 0) {
+        console.log("To negative infinity");
+    } else if (num === 0) {
+        console.log("Staying home");
+    }
+}
+
+beyond(Infinity);
+beyond(17);
+beyond(-19);
+beyond(0);
